b74a6f24d0fa520169160e3c2f8f47a8b13c7047
Mikalai <example@gmail.com>
2024-11-26 05:23:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert();
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you here?", "d");
+// console.log(answer);
+// console.log(typeof(answer));
+
+// const category = "toys";
+
+// console.log(`https://url.com/${category}`);
+console.log(0.5 * 2 === 1);
